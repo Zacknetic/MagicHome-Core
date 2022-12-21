c21fbc9615eb9fb7ea4ff507d6b49f60890bb0b1
Zacknetic <zackavino@gmail.com>
2022-12-21 01:21:57 -0500

    Updated testing with nodemon to handle pesky async Updated deviceInterface class to handle return vals

--- src/DeviceInterface.ts
+++ src/DeviceInterface.ts
@@ -5,94 +5,94 @@ import { commandToByteArray, isStateEqual } from './utils/coreUtils';
 
 import { bufferToDeviceResponse as bufferToCompleteResponse } from './utils/miscUtils';
 
-const RETRY_WAIT_MS = 2000;
-
+const RETRY_WAIT_MS = 3000;
 
 export class DeviceInterface {
+    // Declare a global variable to store the resolve function of the Promise
+    // returned by the first call to sendCommand
+    private sendCommandResolve: (value?: ICompleteResponse | PromiseLike<ICompleteResponse>) => void;
+
+    // Declare a global variable to store the Promise returned by the first call to sendCommand
+    private sendCommandPromise: Promise<ICompleteResponse>;
 
     protected queue;
     protected queueSize = 0;
 
     protected latestPowerCommand = null;
-    protected testPowerStateTimeout: NodeJS.Timeout;
-    protected retryCommandTimeout: NodeJS.Timeout = null;
-    protected latestPromiseReject;
     protected transport: Transport;
 
     protected commandQueue: Array<{ deviceCommand: IDeviceCommand, commandOptions: ICommandOptions }>;
     private processingCommand: boolean;
     private retryCount: number;
-
+    protected processing = false;
+    protected timeoutId;
+    resolve: (value: unknown) => void;
     constructor(transport: Transport) {
         console.time('sendCommand')
         this.transport = transport;
-        this.latestPromiseReject = null;
         this.commandQueue = [];
         this.processingCommand = false;
         this.retryCount = 0;
-
     }
 
     public async queryState(timeoutMS = 100): Promise<ICompleteResponse> {
-
-        const responseMsg: Buffer = await this.transport.queryState(timeoutMS).then(res => res).catch(e => { throw e });
-        // if (typeof responseMsg == 'undefined') throw ('updated Response undefiend')
-
+        const responseMsg: Buffer = await this.transport.queryState(timeoutMS).catch(e => { throw e });
         const completeResponse: ICompleteResponse = bufferToCompleteResponse(responseMsg);
         return completeResponse;
-
     }
 
-    public async sendCommand(deviceCommand: IDeviceCommand, commandOptions: ICommandOptions): Promise<ICompleteResponse> {
-        this.commandQueue.push({ deviceCommand, commandOptions });
-        if (!this.processingCommand) {
-            this.processingCommand = true;
-            let result: ICompleteResponse;
-            while (this.commandQueue.length > 0) {
-                const { deviceCommand: _deviceCommand, commandOptions: _commandOptions } = this.commandQueue.shift();
-                result = await this.processCommand(_deviceCommand, _commandOptions);
+    public async sendCommand(deviceCommand: IDeviceCommand, commandOptions: ICommandOptions) {
+
+        this.sendCommandToTransport(deviceCommand, commandOptions);
+        if (this.processing) this.resolve('exited early')
+        return new Promise(async (_resolve, _reject) => {
+            this.resolve = _resolve;
+            if (this.processing) {
+                this.abort();
             }
-            this.processingCommand = false;
-            return result;
-        } else {
-            return null;
-        }
+            this.processing = true;
+            this.timeoutId = setTimeout(async () => {
+                this.processing = false;
+                const result: ICompleteResponse | void = await this.processCommand(deviceCommand, commandOptions).catch(e => { _reject(e) });
+                _resolve(result);
+            }, 500);
+        }).catch(e => { throw e });
+
     }
+
 
 
     private async processCommand(deviceCommand: IDeviceCommand, commandOptions: ICommandOptions): Promise<ICompleteResponse> {
         try {
             let state: ICompleteResponse = null;
-            let retries = commandOptions.maxRetries;
             let isValidState = false;
-
-            do {
-                try {
+            let retryCount = commandOptions.maxRetries || 0;
+            while (!isValidState && retryCount > 0) {
+                await new Promise((resolve) => setTimeout(resolve, 3000));
+                state = await this.queryState(500);
+                isValidState = isStateEqual(deviceCommand, state, commandOptions.commandType);
+                if (!isValidState) {
                     await this.sendCommandToTransport(deviceCommand, commandOptions);
-
-                    if (this.commandQueue.length !== 0) {
-                        return null;
-                    }
-                    await new Promise(resolve => setTimeout(resolve, RETRY_WAIT_MS));
-
-                    state = await this.queryState(500);
-                    isValidState = await isStateEqual(deviceCommand, state, commandOptions.commandType);
-                } catch (e) {
-                    //do nothing just catch the error
                 }
-                retries--;
-            } while (!isValidState)
+                retryCount--;
+            }
+            if (!isValidState) {
+                throw new Error('Invalid state');
+            }
             return state;
         } catch (e) {
             throw e;
         }
     }
 
-
-
     private async sendCommandToTransport(deviceCommand: IDeviceCommand, commandOptions: ICommandOptions): Promise<void> {
         console.timeLog('sendCommand');
         const byteArray = commandToByteArray(deviceCommand, commandOptions);
         await this.transport.quickSend(byteArray);
+    }
+
+    abort() {
+        clearTimeout(this.timeoutId);
+        this.processing = false;
     }
 }
