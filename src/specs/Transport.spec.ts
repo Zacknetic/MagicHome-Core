26edf97d9b668c8ff8046407b8a5db782eb07f4f
Zacknetic <zackavino@gmail.com>
2022-12-26 12:51:23 -0500

    testing new transport

--- src/specs/Transport.spec.ts
+++ src/specs/Transport.spec.ts
@@ -1,83 +1,85 @@
-// import { Transport } from '../Transport'
+import { Transport } from '../Transport'
 
-// const TEST_MODE = true
-// const HOST = '192.168.1.38'
-// import * as types from '../types'
+const TEST_MODE = true
+const HOST = '192.168.50.137'
+import * as types from '../types'
 
-// const {
-//     DEVICE_COMMAND_BYTES: { COMMAND_POWER_OFF, COMMAND_POWER_ON, COMMAND_QUERY_STATE },
-//     COMMAND_TYPE: { POWER_COMMAND, COLOR_COMMAND, ANIMATION_FRAME, QUERY_COMMAND },
-// } = types;
+const {
+    DEVICE_COMMAND_BYTES: { COMMAND_POWER_OFF, COMMAND_POWER_ON, COMMAND_QUERY_STATE },
+    COMMAND_TYPE: { POWER_COMMAND, COLOR_COMMAND, ANIMATION_FRAME, QUERY_COMMAND },
+} = types;
 
-// const red = [0x31, 0xFF, 0x00, 0x00, 0x00, 0xF0, 0x0F];
-// const green = [0x31, 0x00, 0xFF, 0x00, 0x00, 0xF0, 0x0F];
-// const blue = [0x31, 0x00, 0x00, 0xFF, 0x00, 0xF0, 0x0F];
-// // const red = [0x31, 0xFF, 0x00, 0x00, 0x00, 0x00, 0xF0, 0x0F];
-// // const green = [0x31, 0x00, 0xFF, 0x00, 0x00, 0x00, 0xF0, 0x0F];
-// // const blue = [0x31, 0x00, 0x00, 0xFF, 0x00, 0x00, 0xF0, 0x0F];
-// // const warmWhite = [0x31, 0x00, 0x00, 0xFF, 0x00, 0x00, 0xF0, 0x0F];
+const red = [0x31, 0xFF, 0x00, 0x00, 0x00, 0xF0, 0x0F];
+const green = [0x31, 0x00, 0xFF, 0x00, 0x00, 0xF0, 0x0F];
+const blue = [0x31, 0x00, 0x00, 0xFF, 0x00, 0xF0, 0x0F];
+// const red = [0x31, 0xFF, 0x00, 0x00, 0x00, 0x00, 0xF0, 0x0F];
+// const green = [0x31, 0x00, 0xFF, 0x00, 0x00, 0x00, 0xF0, 0x0F];
+// const blue = [0x31, 0x00, 0x00, 0xFF, 0x00, 0x00, 0xF0, 0x0F];
+// const warmWhite = [0x31, 0x00, 0x00, 0xFF, 0x00, 0x00, 0xF0, 0x0F];
 
-// const power = COMMAND_POWER_ON;
+const power = COMMAND_POWER_ON;
 
-// const TIMEOUT = 1000;
-// let transport;
-// // ; (async () => {
-// // const testParams = {
-// //     result: 'FAIL',
-// //     timeToFail: '500'
-// // }
-// // transport = new Transport(HOST);
-
-// before(done => {
-//     setTimeout(done, 100);
-//     transport = new Transport(HOST);
-
-// });
-// async function asyncMethod(byteArray, expectResponse, timeout?) {
-//     transport.quickSend(byteArray, timeout, expectResponse);2
-//      return await transport.queryState(1000);
+const TIMEOUT = 1000;
+let transport;
+// ; (async () => {
+// const testParams = {
+//     result: 'FAIL',
+//     timeToFail: '500'
 // }
+// transport = new Transport(HOST);
 
-// // describe('Check input data types', () => {
+before(done => {
+    setTimeout(done, 100);
+    transport = new Transport(HOST);
+});
+async function asyncMethod(byteArray, expectResponse, timeout?) {
+    transport.quickSend(byteArray, timeout, expectResponse);
+    return await transport.queryState(1000);
+}
 
-// //     it('Should accept format: byteArray: number[]', () => {
+// describe('Check input data types', () => {
 
-// //         asyncMethod(COMMAND_QUERY_STATE, true, TIMEOUT).then((returnValue) => {
-// //             console.log('Current State: ', returnValue)
-// //             // assert.strictEqual((returnValue !== null), true) //TODO create real assert
-// //         }).catch(err => {
-// //             console.log('FATAL err', err)
-// //         })
+//     it('Should accept format: byteArray: number[]', () => {
 
-
-// //     })
-
-// //     it('Should handle unsupported format', () => {
-// //         asyncMethod("THIS IS A STRING WHICH IS UNSUPORTED", true, TIMEOUT).then((returnValue) => {
-// //             console.log('Test return value:', returnValue)
-// //         }).catch(err => {
-// //             // assert(typeof err == typeof new Error())
-// //             console.log('Successfully caught unsuported format')
-// //         })
-
-// //     })
-// // })
+//         asyncMethod(COMMAND_QUERY_STATE, true, TIMEOUT).then((returnValue) => {
+//             console.log('Current State: ', returnValue)
+//             // assert.strictEqual((returnValue !== null), true) //TODO create real assert
+//         }).catch(err => {
+//             console.log('FATAL err', err)
+//         })
 
 
-// // describe('Query State Test', () => {
+//     })
 
-// //     it('Should return current device state', () => {
+//     it('Should handle unsupported format', () => {
+//         asyncMethod("THIS IS A STRING WHICH IS UNSUPORTED", true, TIMEOUT).then((returnValue) => {
+//             console.log('Test return value:', returnValue)
+//         }).catch(err => {
+//             // assert(typeof err == typeof new Error())
+//             console.log('Successfully caught unsuported format')
+//         })
 
-// //         asyncMethod(COMMAND_QUERY_STATE, true, TIMEOUT).then((returnValue) => {
-// //             console.log('return value: ', returnValue)
-// //         }).catch(err => {
-// //             console.log('FATAL err', err)
-// //         })
+//     })
+// })
 
 
-// //     })
+describe('Query State Test', () => {
 
-// // });
+    it('Should return current device state', () => {
+
+        transport.queryState(1000).then((returnValue) => {
+            console.log('Current State: ', returnValue)
+            // assert.strictEqual((returnValue !== null), true) //TODO create real assert
+        }
+        ).catch(err => {
+            console.log('FATAL err', err)
+        }
+        )
+
+
+    })
+
+});
 
 
 // describe('Send Command Test', () => {
@@ -116,11 +118,11 @@
 // });
 
 
-// async function sleep(ms) {
-//     return new Promise((resolve) => {
-//         setTimeout(resolve, ms);
-//     });
-// }
+async function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
 
-// // https://www.testim.io/blog/testing-promises-using-mocha/
+// https://www.testim.io/blog/testing-promises-using-mocha/
 
