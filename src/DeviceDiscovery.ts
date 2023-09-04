3085e16e5bcb9491f3e619cfd9868f65f30e812f
Zacknetic <zackavino@gmail.com>
2023-09-04 16:49:05 -0400

    improved discovery parsing and tests

--- src/DeviceDiscovery.ts
+++ src/DeviceDiscovery.ts
@@ -1,64 +1,84 @@
-import * as dgram from "dgram";
-import { Network } from "./Network";
-import { DeviceInterface } from "./DeviceInterface";
-import { Transport } from "./Transport";
-import { IProtoDevice, ICompleteDevice, ICompleteResponse, ICompleteDeviceInfo, DEFAULT_COMPLETE_RESPONSE, IFetchStateResponse } from "./types";
-import { mergeDeep, cloneDeep, combineDeep, sleepTimeout } from "./utils/miscUtils";
-import { generateCompleteResponse } from "./utils/MHResponses";
+import * as dgram from 'dgram';
+import * as net from 'net';
+import { Network } from './Network';
+import { DeviceInterface } from './DeviceInterface';
+import { Transport } from './Transport';
+import {
+	IProtoDevice,
+	ICompleteDevice,
+	ICompleteResponse,
+	ICompleteDeviceInfo,
+	DEFAULT_COMPLETE_RESPONSE,
+	IFetchStateResponse,
+} from './types';
+import {
+	mergeDeep,
+	cloneDeep,
+	combineDeep,
+	sleepTimeout,
+} from './utils/miscUtils';
+import { generateCompleteResponse } from './utils/MHResponses';
 
 const BROADCAST_PORT: number = 48899;
-const BROADCAST_MAGIC_STRING: string = "HF-A11ASSISTHREAD";
+const BROADCAST_MAGIC_STRING: string = 'HF-A11ASSISTHREAD';
 
-export async function discoverDevices(timeout = 1000, customSubnets: string[] = []): Promise<IProtoDevice[]> {
-  const userInterfaces: string[] = [];
+let testCounter = 0;
+export async function discoverDevices(
+	timeout = 1000,
+	customSubnets: string[] = []
+): Promise<IProtoDevice[]> {
+	const userInterfaces: string[] = [];
 
-  if (dockerHostMode()) {
-    userInterfaces.push("255.255.255.255");
-  } else {
-    for (const subnet of Network.subnets()) {
-      userInterfaces.push(subnet.broadcast);
+	if (dockerHostMode()) {
+		userInterfaces.push('255.255.255.255');
+	} else {
+		for (const subnet of Network.subnets()) {
+			userInterfaces.push(subnet.broadcast);
+		}
+		userInterfaces.push(...customSubnets);
+	}
+
+	const protoDevicesSet = new Set<string>();
+	const protoDevicesList: IProtoDevice[] = [];
+	const socket: dgram.Socket = dgram.createSocket({
+		type: 'udp4',
+		reuseAddr: true,
+	});
+
+	socket.bind(BROADCAST_PORT);
+
+	socket.on('error', (err) => {
+		socket.close();
+		return err;
+	});
+
+	socket.on('listening', () => {
+		socket.setBroadcast(true);
+		for (const userInterface of userInterfaces) {
+			socket.send(BROADCAST_MAGIC_STRING, BROADCAST_PORT, userInterface);
+		}
+	});
+
+	socket.on('message', (msg, rinfo) => {
+    if(testCounter > 3) return;
+		const parts = msg.toString().split(',');
+		const [ipAddress, uniqueId, modelNumber] = parts;
+		if ((net.isIPv4(ipAddress) || net.isIPv6(ipAddress)) && !protoDevicesSet.has(uniqueId)) {
+			protoDevicesList.push({
+				ipAddress,
+				uniqueId,
+				modelNumber,
+			});
+		} else {
+      //TODO: handle discovery errors
     }
-    userInterfaces.push(...customSubnets);
-  }
+	});
 
-  const protoDevicesSet = new Set<string>();
-  const protoDevicesList: IProtoDevice[] = [];
-  const socket: dgram.Socket = dgram.createSocket({ type: "udp4", reuseAddr: true });
-
-  socket.bind(BROADCAST_PORT);
-
-  socket.on("error", (err) => {
-    socket.close();
-    return err;
-  });
-
-  socket.on("listening", () => {
-    socket.setBroadcast(true);
-    for (const userInterface of userInterfaces) {
-      socket.send(BROADCAST_MAGIC_STRING, BROADCAST_PORT, userInterface);
-    }
-  });
-
-  socket.on("message", (msg, rinfo) => {
-    const address = rinfo.address;
-    const parts = msg.toString().split(",");
-    if (parts.length !== 3) return;
-
-    const [ipAddress, uniqueId, modelNumber] = parts;
-    if (protoDevicesSet.has(uniqueId)) return;
-
-    protoDevicesList.push({
-      ipAddress,
-      uniqueId,
-      modelNumber,
-    });
-  });
-
-  await sleepTimeout(timeout).catch((e) => {
-    throw "sleep somehow failed";
-  });
-  socket.close();
-  return protoDevicesList;
+	await sleepTimeout(timeout).catch((e) => {
+		throw 'sleep somehow failed';
+	});
+	socket.close();
+	return protoDevicesList;
 }
 
 /**
@@ -68,54 +88,74 @@ export async function discoverDevices(timeout = 1000, customSubnets: string[] = 
  * @returns completeDevice[]
  */
 
-export async function completeDevices(protoDevices: IProtoDevice[], timeout = 500, retries = 3): Promise<ICompleteDevice[]> {
-  //there is a potential bug here where if protodevices doesn't exist it will throw an error.
-  //to fix it we need to check if protodevices exists before we do anything else.
-  //this is how
-  //if (!protoDevices) return;
-  if (!protoDevices || protoDevices.length < 1) return;
+export async function completeDevices(
+	protoDevices: IProtoDevice[],
+	timeout = 500,
+	retries = 3
+): Promise<ICompleteDevice[]> {
 
-  const deviceResponses: Promise<ICompleteDevice>[] = [];
-  const completedDevices: ICompleteDevice[] = [];
-  const retryProtoDevices: IProtoDevice[] = [];
+	if (!protoDevices || protoDevices.length < 1) return;
 
-  protoDevices.forEach((protoDevice) => {
-    deviceResponses.push(
-      completeDevice(protoDevice, timeout).catch((e) => {
-        throw Error("DeviceDiscoveryError: " + e)
-      })
-    );
-  });
+	const deviceResponses: Promise<ICompleteDevice>[] = [];
+	const completedDevices: ICompleteDevice[] = [];
+	const retryProtoDevices: IProtoDevice[] = [];
 
-  await Promise.allSettled(deviceResponses).then((results) => {
-    results.forEach((result) => {
-      if (result.status === "fulfilled") completedDevices.push(result.value);
-      else retryProtoDevices.push(result.reason.protoDevice);
-    });
-  });
-  if (retryProtoDevices.length > 0 && retries > 0)
-    completedDevices.push(
-      ...(await completeDevices(retryProtoDevices, timeout, retries - 1).catch((e) => {
-        throw Error("DeviceDiscoveryError: " + e)
-      }))
-    );
+	protoDevices.forEach((protoDevice) => {
+		deviceResponses.push(
+			completeDevice(protoDevice, timeout).catch((e) => {
+				throw Error('DeviceDiscoveryError: ' + e);
+			})
+		);
+	});
 
-  return completedDevices;
+	await Promise.allSettled(deviceResponses).then((results) => {
+		results.forEach((result) => {
+			if (result.status === 'fulfilled') completedDevices.push(result.value);
+			else retryProtoDevices.push(result.reason.protoDevice);
+		});
+	});
+	if (retryProtoDevices.length > 0 && retries > 0)
+		completedDevices.push(
+			...(await completeDevices(retryProtoDevices, timeout, retries - 1).catch(
+				(e) => {
+					throw Error('DeviceDiscoveryError: ' + e);
+				}
+			))
+		);
+
+	return completedDevices;
 }
 
-async function completeDevice(protoDevice: IProtoDevice, timeout): Promise<ICompleteDevice> {
-  try {
-    const transport = new Transport(protoDevice.ipAddress);
-    const deviceInterface = new DeviceInterface(transport);
+async function completeDevice(
+	protoDevice: IProtoDevice,
+	timeout
+): Promise<ICompleteDevice> {
+	try {
+		const transport = new Transport(protoDevice.ipAddress);
+		const deviceInterface = new DeviceInterface(transport);
 
-    const fetchStateResponse: IFetchStateResponse = await deviceInterface.queryState(timeout);
-    const completeDeviceInfo: ICompleteDeviceInfo = { deviceMetaData: fetchStateResponse.deviceMetaData, protoDevice, latestUpdate: Date.now() };
-    const completeResponse: ICompleteResponse = generateCompleteResponse({fetchStateResponse, responseCode: 1, deviceCommand: null, responseMsg: "completeDevice"});
-    const completeDevice: ICompleteDevice = {completeResponse, deviceInterface, completeDeviceInfo };
-    return completeDevice;
-  } catch (e) {
-    throw Error("completeDeviceError: " + e)
-  }
+		const fetchStateResponse: IFetchStateResponse =
+			await deviceInterface.queryState(timeout);
+		const completeDeviceInfo: ICompleteDeviceInfo = {
+			deviceMetaData: fetchStateResponse.deviceMetaData,
+			protoDevice,
+			latestUpdate: Date.now(),
+		};
+		const completeResponse: ICompleteResponse = generateCompleteResponse({
+			fetchStateResponse,
+			responseCode: 1,
+			deviceCommand: null,
+			responseMsg: 'completeDevice',
+		});
+		const completeDevice: ICompleteDevice = {
+			completeResponse,
+			deviceInterface,
+			completeDeviceInfo,
+		};
+		return completeDevice;
+	} catch (e) {
+		throw Error('completeDeviceError: ' + e);
+	}
 }
 
 /**
@@ -123,25 +163,33 @@ async function completeDevice(protoDevice: IProtoDevice, timeout): Promise<IComp
  * @param timeout number
  * @returns completeDevice[]
  */
-export function completeCustomDevices(completeDevicesInfo: ICompleteDeviceInfo[]): ICompleteDevice[] {
-  const completeDevices: ICompleteDevice[] = [];
+export function completeCustomDevices(
+	completeDevicesInfo: ICompleteDeviceInfo[]
+): ICompleteDevice[] {
+	const completeDevices: ICompleteDevice[] = [];
 
-  for (const completeDeviceInfo of completeDevicesInfo) {
-    const {
-      protoDevice: { ipAddress },
-    } = completeDeviceInfo;
+	for (const completeDeviceInfo of completeDevicesInfo) {
+		const {
+			protoDevice: { ipAddress },
+		} = completeDeviceInfo;
 
-    const transport = new Transport(ipAddress);
-    const deviceInterface = new DeviceInterface(transport);
+		const transport = new Transport(ipAddress);
+		const deviceInterface = new DeviceInterface(transport);
 
-    const completeResponse: ICompleteResponse = cloneDeep(DEFAULT_COMPLETE_RESPONSE);
-    const completeDevice: ICompleteDevice = { completeDeviceInfo, deviceInterface, completeResponse };
-    completeDevices.push(completeDevice);
-  }
+		const completeResponse: ICompleteResponse = cloneDeep(
+			DEFAULT_COMPLETE_RESPONSE
+		);
+		const completeDevice: ICompleteDevice = {
+			completeDeviceInfo,
+			deviceInterface,
+			completeResponse,
+		};
+		completeDevices.push(completeDevice);
+	}
 
-  return completeDevices;
+	return completeDevices;
 }
 
 function dockerHostMode(): boolean {
-  return !!process.env.DOCKER_HOST_NETWORK;
+	return !!process.env.DOCKER_HOST_NETWORK;
 }
--- src/specs/DeviceDiscovery.spec.ts
+++ src/specs/DeviceDiscovery.spec.ts
@@ -1,68 +1,73 @@
 // import { DeviceInterface } from '../DeviceInterface'
 // import { ICommandOptions, ICommandResponse, IDeviceCommand } from '../types';
 
-import assert = require("assert");
-import { discoverDevices, completeDevices } from "../DeviceDiscovery";
-import { ICompleteDevice } from "../types";
-import { ICommandOptions } from "../types";
-import * as types from "../types";
+import assert = require('assert');
+import { discoverDevices, completeDevices } from '../DeviceDiscovery';
+import { ICompleteDevice } from '../types';
+import { ICommandOptions } from '../types';
+import * as types from '../types';
 // import * as types from '../types'
 
-describe("Test DeviceDiscovery class functions", function () {
-  const {
-    COMMAND_TYPE: { COLOR_COMMAND },
-  } = types;
+describe('Test DeviceDiscovery class functions', function () {
+	const {
+		COMMAND_TYPE: { COLOR_COMMAND },
+	} = types;
 
-  describe("Test the DeviceInterface class", () => {
-    const commandOptions: ICommandOptions = {
-      colorAssist: false,
-      waitForResponse: true,
-      maxRetries: 5,
-      remainingRetries: 5,
-      commandType: COLOR_COMMAND,
-      timeoutMS: 50,
-      isEightByteProtocol: true,
-    };
+	describe('Test the DeviceInterface class', () => {
+		const commandOptions: ICommandOptions = {
+			colorAssist: false,
+			waitForResponse: true,
+			maxRetries: 5,
+			remainingRetries: 5,
+			commandType: COLOR_COMMAND,
+			timeoutMS: 50,
+			isEightByteProtocol: true,
+		};
 
-    it("Should find all devices, create CompleteDevice, and set light state to red", async function () {
-      // Discover devices
-      const protoDevices = await discoverDevices();
-      assert.strictEqual(protoDevices.length > 0, true);
+		let protoDevices: types.IProtoDevice[] = [];
+		let completedDevices: ICompleteDevice[] = [];
+		it('Should find all devices, create CompleteDevice, and set light state to red', async function () {
+			// Discover devices
+			protoDevices = await discoverDevices();
+			console.log(protoDevices);
+			assert.strictEqual(protoDevices.length > 0, true);
+		});
 
-      // Create CompleteDevice for each ProtoDevice
-      const completedDevices: ICompleteDevice[] = await completeDevices(
-        protoDevices,
-        500,
-        10
-      );
-      assert.strictEqual(protoDevices.length, completedDevices.length);
+		it('Should create the same number of CompleteDevice for each ProtoDevice.', async function () {
+			// Create CompleteDevice for each ProtoDevice
+			completedDevices = await completeDevices(protoDevices, 500, 10);
+			console.log(completedDevices);
+			assert.strictEqual(protoDevices.length, completedDevices.length);
+		});
 
-      // Define the red command
-      const deviceCommand = {
-        isOn: true,
-        RGB: { red: 255, green: 0, blue: 0 },
-        CCT: { warmWhite: 0, coldWhite: 0 },
-        colorMask: 0xf0,
-      };
+		it('Should set light state to red for each device.', async function () {
+			// Define the red command
+			const deviceCommand = {
+				isOn: true,
+				RGB: { red: 255, green: 0, blue: 0 },
+				CCT: { warmWhite: 0, coldWhite: 0 },
+				colorMask: 0xf0,
+			};
 
-      // Create an array of promises to send the red command to each device
-      const promises = completedDevices.map(async (device) => {
-        try {
-          const returnValue: types.ICompleteResponse | void =
-            await device.deviceInterface.sendCommand(
-              deviceCommand,
-              commandOptions
-            );
-        } catch (err) {
-        //   console.log(
-        //     `Device ${device.completeDeviceInfo.protoDevice.ipAddress} error: `,
-        //     err
-        //   );
-        }
-      });
+			// Create an array of promises to send the red command to each device
+			const promises = completedDevices.map(async (device) => {
+				try {
+					const returnValue: types.ICompleteResponse | void =
+						await device.deviceInterface.sendCommand(
+							deviceCommand,
+							commandOptions
+						);
+					console.log('return value: ', returnValue);
+				} catch (err) {
+					console.log(
+						`Device ${device.completeDeviceInfo.protoDevice.ipAddress} error: `,
+						err
+					);
+				}
+			});
 
-      // Wait for all promises to complete
-      await Promise.all(promises);
-    });
-  });
+			// Wait for all promises to complete
+			await Promise.all(promises);
+		});
+	});
 });
